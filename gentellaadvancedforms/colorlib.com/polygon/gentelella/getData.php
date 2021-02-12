23839c4d5cce2bd570cb2808d7e12975edb3bb05
Rodrigo Villarroel <rodrigo.villarroel.m@gmail.com>
2021-02-12 15:39:39 -0400

    phps

--- gentellaadvancedforms/colorlib.com/polygon/gentelella/getData.php
+++ gentellaadvancedforms/colorlib.com/polygon/gentelella/getData.php
@@ -7,17 +7,53 @@ $opts = array(
 );
 
 $context = stream_context_create($opts);
-$x = file_get_contents("http://sistema.mym.com.bo:4000/trabajador/empresa/" . $_SESSION['CI'], false, $context);
+$x = file_get_contents("http://sistema.mym.com.bo:4000/trabajador/empresa/" . $_SESSION['EMPRESA'], false, $context);
 $someArray = json_decode($x, true);
 foreach ($someArray as $key => $value) {
     echo "<tr>";
-    echo "<td>" . $value["IdBien"] . "</td>";
-    echo "<td>" . $value["UbicacionD"] . "</td>";
-    echo "<td>" . $value["ClasificacionD"] . "</td>";
-    echo "<td>" . $value["EstadoD"] . "</td>";
-    echo "<td>" . $value["Responsable"] . "</td>";
-    echo "<td>" . $value["Calificacion"] . "</td>";
-    echo "<td>" . $value["Precio"] . " </td>";
+    echo "<td>" . $value["ci"] . "</td>";
+    echo "<td>" . $value["nombre"] . " " . $value["primer_apellido"] . " " . $value["segundo_apellido"] . "</td>";
+    echo "<td>" . $value["correo_electronico"] . "</td>";
+    echo "<td>" . $value["telefono"] . "</td>";
+    echo "<td>" . "<button type=\"button\" class=\"btn btn-info btn-sm\"><i class=\"fa fa-edit\"></i></button><button type=\"button\" class=\"btn btn-danger btn-sm\"><i class=\"fa fa-trash\"></i></button>" . "</td>";
+    echo "</tr>";
+}
+?>
+
+<?php
+$opts = array(
+    'http' => array(
+        'method' => "GET",
+        'header' => "X-JWT-Token: " . $_SESSION['TOKEN']
+    )
+);
+
+$context = stream_context_create($opts);
+$x = file_get_contents("http://sistema.mym.com.bo:4000/empresa/", false, $context);
+$someArray = json_decode($x, true);
+foreach ($someArray as $key => $value) {
+    echo "<option value=\"" . $value["id"] . "\">" . $value["razon_social"] . "</option>";
+}
+?>
+
+<?php
+$opts = array(
+    'http' => array(
+        'method' => "GET",
+        'header' => "X-JWT-Token: " . $_SESSION['TOKEN']
+    )
+);
+
+$context = stream_context_create($opts);
+$x = file_get_contents("http://sistema.mym.com.bo:4000/trabajador/empresa/" . $_SESSION['EMPRESA'], false, $context);
+$someArray = json_decode($x, true);
+foreach ($someArray as $key => $value) {
+    echo "<tr>";
+    echo "<td>" . $value["ci"] . "</td>";
+    echo "<td>" . $value["nombre"] . " " . $value["primer_apellido"] . " " . $value["segundo_apellido"] . "</td>";
+    echo "<td>" . $value["correo_electronico"] . "</td>";
+    echo "<td>" . $value["telefono"] . "</td>";
+    echo "<td>" . "<button type=\"button\" class=\"btn btn-info btn-sm\"><i class=\"fa fa-edit\"></i></button><button type=\"button\" class=\"btn btn-danger btn-sm\"><i class=\"fa fa-trash\"></i></button>" . "</td>";
     echo "</tr>";
 }
 ?>
